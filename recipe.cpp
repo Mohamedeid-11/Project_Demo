db5babc55592434dcaac10b9a34b928fad064e3a
Mohamed Eid <109903701+Mohamedeid-11@users.noreply.github.com>
2025-04-21 05:06:47 +0200

    Update recipe.cpp

--- recipe.cpp
+++ recipe.cpp
@@ -7,9 +7,6 @@ Recipe::~Recipe() {}
 
 void Recipe::generate_id()
 {
-    if(!num_of_recipes)
-        this->id = 0;
-    else
-        this->id = recipes[num_of_recipes-1]->id + 1;
+    this->id = next_id;
+    next_id++;
 }
-
